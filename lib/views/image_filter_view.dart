d40b530ad79fc6d80a118f10a7550683428a4cf8
lsmnt <leasimonet56@gmail.com>
2022-09-08 00:28:21 +0200

    clean filter view

--- lib/views/image_filter_view.dart
+++ lib/views/image_filter_view.dart
@@ -14,78 +14,26 @@ class ImageFilter extends StatefulWidget {
   State<ImageFilter> createState() => _ImageFilterState();
 }
 
-//Charge l'image et les textBlocs dans le build
-List<Widget> showPositioned(BuildContext context, int recognizedId) {
-  var recognzers = context.read<Recognizers>();
-  Recognizer recognizer = recognzers.getRecognizers()[recognizedId];
+double width = 0;
+double height = 0;
 
-  double width = MediaQuery.of(context).size.width;
-  double height = MediaQuery.of(context).size.height;
+late Recognizers recognzers;
+late Recognizer recognizer;
 
-  double aspectRationWidth = recognizer.widthImage / width;
-  double aspectRationHeight = recognizer.heightImage / height;
-
-  List<Widget> positioned = List.empty(growable: true);
-
-  positioned = recognizer
-      .getTextBlock()
-      .map((textBlock) => Positioned(
-            left: textBlock.boundingBox.left.toDouble() / aspectRationWidth,
-            top: textBlock.boundingBox.top.toDouble() / aspectRationHeight,
-            width: textBlock.boundingBox.width.toDouble() / aspectRationWidth,
-            height:
-                textBlock.boundingBox.height.toDouble() / aspectRationHeight,
-            child: GestureDetector(onTap: () {
-              saveTextBlock(context, recognizedId,
-                  recognizer.getTextBlock().indexOf(textBlock));
-            }, onPanUpdate: (details) {
-              saveTextBlock(context, recognizedId,
-                  recognizer.getTextBlock().indexOf(textBlock));
-            }, child:
-                Consumer<Recognizers>(builder: (context, recognizers, child) {
-              bool isSaved = recognizer.getSavedTextBlock().contains(textBlock);
-
-              return IgnorePointer(
-                ignoring: isSaved,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: isSaved
-                        ? Color.fromARGB(30, 16, 16, 16)
-                        : Color.fromARGB(125, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(5),
-                    border: Border.all(
-                        color: Colors.white, width: 0.5 // red as border color
-                        ),
-                  ),
-                ),
-              );
-            })),
-          ))
-      .toList();
-
-  return positioned;
-}
-
-void saveTextBlock(BuildContext context, int recognizedId, int textBlockId) {
-  var recognzers = context.read<Recognizers>();
-  Recognizer recognizer = recognzers.getRecognizers()[recognizedId];
-
-  recognizer.addSavedTextBlock(textBlockId);
-
-  Provider.of<Recognizers>(context, listen: false).refreshRecognizers();
-}
+double aspectRationWidth = 1;
+double aspectRationHeight = 1;
 
 class _ImageFilterState extends State<ImageFilter> {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
 
-    var recognzers = context.read<Recognizers>();
-    Recognizer recognizer = recognzers.getRecognizers()[widget.recognizedId];
+    recognzers = context.read<Recognizers>();
+    recognizer = recognzers.getRecognizers()[widget.recognizedId];
 
-    double aspectRationWidth = recognizer.widthImage / width;
-    double aspectRationHeight = recognizer.heightImage / height;
+    aspectRationWidth = recognizer.widthImage / width;
+    aspectRationHeight = recognizer.heightImage / height;
 
     return Scaffold(
         body: Listener(
@@ -94,22 +42,69 @@ class _ImageFilterState extends State<ImageFilter> {
             event.position.dx * aspectRationWidth,
             event.position.dy * aspectRationHeight);
         if (textBlock != null) {
-          recognizer
-              .addSavedTextBlock(recognizer.getTextBlock().indexOf(textBlock));
-          Provider.of<Recognizers>(context, listen: false).refreshRecognizers();
+          saveTextBlock(context, recognizer.getTextBlock().indexOf(textBlock));
         }
       },
       child: Container(
-        child: Stack(
-          children: showPositioned(context, widget.recognizedId),
-        ),
         decoration: BoxDecoration(
           image: DecorationImage(
             fit: BoxFit.cover,
             image: Image.file(File(recognizer.getFilePath())).image,
           ),
         ),
+        child: Stack(
+          children: showPositioned(context, widget.recognizedId),
+        ),
       ),
     ));
+  }
+
+  //Charge les textBlocs en positionned
+  List<Widget> showPositioned(BuildContext context, int recognizedId) {
+    List<Widget> positioned = List.empty(growable: true);
+
+    positioned = recognizer
+        .getTextBlock()
+        .map((textBlock) => Positioned(
+              left: textBlock.boundingBox.left.toDouble() / aspectRationWidth,
+              top: textBlock.boundingBox.top.toDouble() / aspectRationHeight,
+              width: textBlock.boundingBox.width.toDouble() / aspectRationWidth,
+              height:
+                  textBlock.boundingBox.height.toDouble() / aspectRationHeight,
+              child: GestureDetector(onTap: () {
+                saveTextBlock(
+                    context, recognizer.getTextBlock().indexOf(textBlock));
+              }, onPanUpdate: (details) {
+                saveTextBlock(
+                    context, recognizer.getTextBlock().indexOf(textBlock));
+              }, child:
+                  Consumer<Recognizers>(builder: (context, recognizers, child) {
+                bool isSaved =
+                    recognizer.getSavedTextBlock().contains(textBlock);
+
+                return IgnorePointer(
+                  ignoring: isSaved,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isSaved
+                          ? Color.fromARGB(30, 16, 16, 16)
+                          : Color.fromARGB(125, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                          color: Colors.white, width: 0.5 // red as border color
+                          ),
+                    ),
+                  ),
+                );
+              })),
+            ))
+        .toList();
+
+    return positioned;
+  }
+
+  void saveTextBlock(BuildContext context, int textBlockId) {
+    recognizer.addSavedTextBlock(textBlockId);
+    Provider.of<Recognizers>(context, listen: false).refreshRecognizers();
   }
 }
